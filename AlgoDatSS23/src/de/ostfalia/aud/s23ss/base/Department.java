e2565cb45ceeeafb8430d90934301e6528daa809
Oliver Greiner-Petter <contact@greiner-petter.net>
2023-03-24 12:25:39 +0000

    a1

---
+++ AlgoDatSS23/src/de/ostfalia/aud/s23ss/base/Department.java
@@ -0,0 +1,24 @@
+package de.ostfalia.aud.s23ss.base;
+
+public enum Department {
+    SERVICE("Service"),
+    DEVELOPMENT("Development"),
+    SALES("Sales"),
+    PRODUCTION("Production"),
+    MANPOWER("Manpower"),
+    RESEARCH("Research"),
+    MARKETING("Marketing"),
+    MANAGEMENT("Management"),
+    FINANCE("Finance");
+
+    private final String name;
+
+    Department(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
---
+++ AlgoDatSS23/src/de/ostfalia/aud/s23ss/base/Gender.java
@@ -0,0 +1,6 @@
+package de.ostfalia.aud.s23ss.base;
+
+public enum Gender {
+    M,
+    F;
+}
