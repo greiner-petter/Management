cfec662cd29fe4128c54c491d8cd6440ff50677a
id163018 <id163018@ce0d7295-9dce-489c-a1be-d78f26f95ecf>
2023-04-26 13:52:12 +0000

    a2.14

--- Gruppe022/AlgoDatSS23/src/de/ostfalia/aud/s23ss/a2/Management.java
+++ Gruppe022/AlgoDatSS23/src/de/ostfalia/aud/s23ss/a2/Management.java
@@ -9,9 +9,10 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Management implements IManagement {
-    private IEmployee[] data = new IEmployee[8];
-    private int operations;
-    private MergeSort merge;
+
+    protected IEmployee[] employees = new IEmployee[8];
+    protected int operations;
+    protected MergeSort mergeSort;
 
     public Management(String fileName) throws IOException {
         operations = 0;
@@ -21,70 +22,80 @@ public class Management implements IManagement {
         int i = 0;
         while (scan.hasNextLine()) {
             String nextLine = scan.nextLine();
-            data[i] = new Employee(nextLine);
+            employees[i] = new Employee(nextLine);
             i++;
-            if (i == data.length) {
-                data = Arrays.copyOf(data, data.length * 2);
+            if (i == employees.length) {
+                employees = Arrays.copyOf(employees, employees.length * 2);
             }
             operations++;
         }
-        data = clearEmptyFields(data);
-        merge = new MergeSort(new KeyComparator());
-        data = merge.sort(data);
-
         fileReader.close();
         scan.close();
+        employees = shorterArray();
+        this.mergeSort = new MergeSort(new KeyComparator());
+        employees = mergeSort.sort(employees, 0, employees.length -1);
     }
 
-    public Management(String[] input) {
-        int i = 0;
-        for (String employee : input) {
-            data[i] = new Employee(employee);
-            i++;
-            if (i == data.length) {
-                data = Arrays.copyOf(data, data.length * 2);
+    public Management(String[] data) {
+        for (int i = 0; i < data.length; i++) {
+            employees[i] = new Employee(data[i]);
+            if (i+1 == employees.length) {
+                employees = Arrays.copyOf(employees, employees.length * 2);
             }
-            operations++;
         }
-        data = clearEmptyFields(data);
-        merge = new MergeSort(new KeyComparator());
-        data = merge.sort(data);
+        employees = shorterArray();
+        this.mergeSort = new MergeSort(new KeyComparator());
+        employees = mergeSort.sort(employees);
     }
 
     public Management() {
-        data = new IEmployee[0];
+        this.employees = new IEmployee[8];
     }
 
     @Override
     public int size() {
-        return data.length;
+        return employees.length;
     }
 
     @Override
     public void insert(IEmployee member) {
         operations = 0;
-        data = Arrays.copyOf(data, data.length + 1);
-        data[data.length - 1] = new Employee(member.toString());
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] == null) {
+                employees[i] = member;
+                operations++;
+                break;
+            }
+        }
+
+        IEmployee[] temp = new IEmployee[employees.length+1];
+        for (int i = 0; i < employees.length; i++) {
+            temp[i] = employees[i];
+        }
+
+        temp[temp.length-1] = member;
+        employees = temp;
         operations++;
-        data = merge.sort(data);
+        employees = shorterArray();
     }
 
     @Override
     public IEmployee search(int key) {
-        merge = new MergeSort(new KeyComparator());
-        data = merge.sort(data);
+        mergeSort = new MergeSort(new KeyComparator());
+        employees = mergeSort.sort(employees, 0, employees.length -1);
         operations = 0;
         int l = 0;
-        int r = data.length;
-        int m;
+        int r = employees.length -1;
+
         while (l <= r) {
-            m = l + ((r - l) / 2);
-            if (data[m].getKey() == key) {
-                return data[m];
-            } else if (data[m].getKey() < key) {
-                r = m - 1;
-            } else {
+            int m = l + (r - l) / 2;
+
+            if (employees[m].getKey() == key) {
+                return employees[m];
+            } else if (employees[m].getKey() < key) {
                 l = m + 1;
+            } else {
+                r = m - 1;
             }
             operations++;
         }
@@ -93,39 +104,38 @@ public class Management implements IManagement {
 
     @Override
     public IEmployee[] search(String name, String firstName) {
-        merge = new MergeSort(new NameComparator());
-        data = merge.sort(data);
+        mergeSort = new MergeSort(new NameComparator());
+        employees = mergeSort.sort(employees, 0, employees.length-1);
         operations = 0;
         IEmployee[] matchingEmployees = new IEmployee[0];
         int i = 0;
         int l = 0;
-        int r = data.length;
+        int r = employees.length;
         int m;
         while (l < r) {
             m = l + ((r - l) / 2);
-            if (data[m].getName().equals(name)) {
-                while (m >= 0 && data[m].getName().equals(name)) {
+            if (employees[m].getName().equals(name)) {
+                while (m >= 0 && employees[m].getName().equals(name)) {
                     m--;
                     operations++;
                 }
                 m++;
-                while (m < data.length && data[m].getName().equals(name)) {
-                    if (data[m].getFirstName().equals(firstName)) {
+                while (m < employees.length && employees[m].getName().equals(name)) {
+                    if (employees[m].getFirstName().equals(firstName)) {
                         if (i == matchingEmployees.length) {
                             matchingEmployees = Arrays.copyOf(matchingEmployees,
-                                                    matchingEmployees.length + 1);
+                                    matchingEmployees.length + 1);
                         }
-                        matchingEmployees[i] = data[m];
+                        matchingEmployees[i] = employees[m];
                         i++;
                     }
                     m++;
                     operations++;
                 }
-                matchingEmployees = clearEmptyFields(matchingEmployees);
                 return matchingEmployees;
-            } else if (data[m].getName().compareTo(name) > 0) {
+            } else if (employees[m].getName().compareTo(name) > 0) {
                 r = m - 1;
-            } else if (data[m].getName().compareTo(name) < 0) {
+            } else {
                 l = m + 1;
             }
             operations++;
@@ -135,60 +145,94 @@ public class Management implements IManagement {
 
     @Override
     public int size(Department department) {
+        mergeSort = new MergeSort(new DepartmentComparator());
+        mergeSort.sort(employees, 0, employees.length-1);
         operations = 0;
-        int counter = 0;
-        for (IEmployee employee : data) {
-            if (employee.getDepartment() == department) {
-                counter++;
-            }
-            operations++;
-        }
-        return counter;
-    }
-
-    @Override
-    public IEmployee[] members(Department department) {
-        merge = new MergeSort(new DepartmentComparator());
-        data = merge.sort(data);
-        operations = 0;
-        IEmployee[] matchingEmployees = new IEmployee[0];
         int i = 0;
         int l = 0;
-        int r = data.length;
+        int r = employees.length;
         int m;
+        int counter = 0;
         while (l < r) {
             m = l + ((r - l) / 2);
-            if (data[m].getDepartment() == department) {
-                while (m >= 0 && data[m].getDepartment() == department) {
+            if (employees[m].getDepartment() == department) {
+                while (m >= 0 && employees[m].getDepartment() == department) {
                     m--;
                     operations++;
                 }
                 m++;
-                while (m < data.length && data[m].getDepartment() == department) {
-                    if (i == matchingEmployees.length) {
-                        matchingEmployees = Arrays.copyOf(matchingEmployees,
-                                matchingEmployees.length + 1);
-                    }
-                    matchingEmployees[i] = data[m];
+                while (m < employees.length && employees[m].getDepartment() == department) {
+                    counter++;
                     i++;
                     m++;
                     operations++;
                 }
-                matchingEmployees = clearEmptyFields(matchingEmployees);
-                return matchingEmployees;
-            } else if (data[m].getDepartment().toString().compareTo(department.toString()) > 0) {
+                return counter;
+            } else if (employees[m].getDepartment().toString().compareTo(department.toString()) > 0) {
                 r = m - 1;
             } else {
                 l = m + 1;
             }
             operations++;
         }
+        return 0;
+    }
+
+    @Override
+    public IEmployee[] members(Department department) {
+        mergeSort = new MergeSort(new DepartmentComparator());
+        employees = mergeSort.sort(employees, 0, employees.length -1);
+        operations = 0;
+        IEmployee[] matchingEmployees = new IEmployee[0];
+
+        if (employees[0].getDepartment() == department) {
+            matchingEmployees = new IEmployee[1];
+            matchingEmployees[0] = employees[0];
+        }
+
+        int i = 0;
+        int l = 0;
+        int r = employees.length;
+        int m;
+        while (l < r) {
+            m = l + ((r - l) / 2);
+            if (employees[m].getDepartment() == department) {
+                while (m >= 0 && employees[m].getDepartment() == department) {
+                    m--;
+                    operations++;
+                }
+                m++;
+                while (m < employees.length && employees[m].getDepartment() == department) {
+                    if (i == matchingEmployees.length) {
+                        matchingEmployees = Arrays.copyOf(matchingEmployees,
+                                matchingEmployees.length + 1);
+                    }
+                    matchingEmployees[i] = employees[m];
+                    i++;
+                    m++;
+                    operations++;
+                }
+                mergeSort = new MergeSort(new KeyComparator());
+                mergeSort.sort(matchingEmployees, 0, matchingEmployees.length- 1);
+                return matchingEmployees;
+            } else if (employees[m].getDepartment().toString().compareTo(department.toString()) > 0) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+            operations++;
+        }
+        mergeSort = new MergeSort(new KeyComparator());
+        mergeSort.sort(matchingEmployees, 0, matchingEmployees.length- 1);
         return matchingEmployees;
     }
 
     @Override
     public IEmployee[] toArray() {
-        return data;
+        employees = shorterArray();
+        this.mergeSort = new MergeSort(new KeyComparator());
+        employees = mergeSort.sort(employees, 0, employees.length -1);
+        return employees;
     }
 
     @Override
@@ -196,36 +240,35 @@ public class Management implements IManagement {
         return operations;
     }
 
-    @Override
-    public int height() {
-        return 0;
-    }
-
-    public IEmployee[] clearEmptyFields(IEmployee[] data) {
-        IEmployee[] clearedData = data.clone();
-        for (IEmployee employee : clearedData) {
-            if (employee == null) {
-                clearedData = Arrays.copyOf(clearedData, clearedData.length - 1);
+    public IEmployee[] shorterArray() {
+        int counter = 0;
+        while (employees[counter] != null) {
+            counter++;
+            if (counter == employees.length) {
+                return employees;
             }
         }
-        return clearedData;
+        IEmployee[] shorter = new IEmployee[counter];
+        for (int i = 0; i < counter; i++) {
+            shorter[i] = employees[i];
+        }
+        return shorter;
     }
 
     public static void main(String[] args) throws IOException {
-//        Management management = new Management(new String[]{
-//                "10049;1961-04-24;Tramer;Basil;F;1992-05-04;Sales",
-//                "10050;1958-05-21;Tramer;Basil;M;1990-12-25;Finance",
-//                "51;1953-07-28;Caine;Hidefumi;M;1992-10-15;Finance",
-//                "10052;1953-07-28;Tramer;Hidefumi;M;1992-10-15;Production",
-//                "10053;1953-07-28;Tramer;Hidefumi;M;1992-10-15;Production",
-//                "10354;1953-07-28;Dredge;Yinghua;M;1992-10-15;Sales",
-//                "10055;1953-07-28;Caine;Yinghua;M;1992-10-15;Sales"
-//        });
-        Management management = new Management("AlgoDatSS23/Materialien/10k_employees.csv");
-//        IEmployee[] ems = management.search("Yinghua", "Caine");
-//        System.out.println(ems.length);
-        for (IEmployee e : management.data) {
-            System.out.println(e);
+        Management management = new Management();
+        Employee em1 = new Employee("10855;1957-08-07;Breannda;Billingsley;F;1991-08-05;Finance");
+        Employee em2 = new Employee("10041;1959-08-27;Uri;Lenart;F;1989-11-12;Sales");
+        Employee em3 = new Employee("10942;1952-08-08;Toshimitsu;Larfeldt;F;1989-09-08;Development");
+
+        management.insert(em1);
+        management.insert(em2);
+        management.insert(em3);
+
+        IEmployee[] mem = management.members(Department.DEVELOPMENT);
+
+        for (IEmployee em : mem) {
+            System.out.println(em);
         }
     }
 }
